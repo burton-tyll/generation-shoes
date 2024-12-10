29e7214ed811229ebbf099091aa611c758399bcb
burton-tyll <burton.tyll@laplateforme.io>
2024-12-10 20:40:20 +0000

    feature: ajout du controller admin/home + ajout des views adminBase et adminHome + ajout du lien vers les panel admin

---
+++ src/Controller/Admin/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+
+    #[Route('/admin/home', name: 'admin_home')]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig');
+    }
+}
